7c673b2b7e264ef822a1c884fc2f984de0cac057
brianmsj <brianmsj@gmail.com>
2017-03-24 11:23:12 -0700

    commiting button inline block changes

--- client/src/components/nav.js
+++ client/src/components/nav.js
@@ -21,17 +21,20 @@ import {Link} from 'react-router';
     render() {
              return (
                 <div className="nav-bar" >
+                 <div className="log-in-container">
                     <h3 className='logged-in-name'>{this.props.name}</h3>
+                    <div className='goog-img'>
+                      <img role="presentation" className="profile-pic" src={this.props.profilePicURL}/>
+                    </div>
+                 </div>
+                 <div className='nav-button'>
                     <div className='inline1'>
                      <Link to={'/Vacations'} className='vacation-link'>All Vacations</Link>
                     </div>
                     <div className='inline1'>
                      <button type="submit" onClick={this.renderInitialState} className='vacation-link'>Logout</button>
                     </div>
-                    <div className='inline1'>
-                      <img role="presentation" className="profile-pic" src={this.props.profilePicURL}/>
-                    </div>
-
+                  </div>
                </div>
         );
     }
